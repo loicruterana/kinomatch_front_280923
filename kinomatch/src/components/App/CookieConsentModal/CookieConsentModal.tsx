78589acfd6063ee02acf19f8dd6ef0903fce03bb
loicruterana <lruterana@gmail.com>
2023-09-28 15:24:37 +0200

    commitkermit

--- kinomatch/src/components/App/CookieConsentModal/CookieConsentModal.tsx
+++ kinomatch/src/components/App/CookieConsentModal/CookieConsentModal.tsx
@@ -16,15 +16,15 @@ function CookieConsentModal() {
     setCookie('cookieConsent', true, { path: '/' });
   };
 
-  if (cookies.cookieConsent) {
-    return null;
-  }
+  // if (cookies.cookieConsent) {
+  //   return null;
+  // }
   
   return (
     <div className="cookie-consent-modal__background">
       <div className="cookie-consent-modal">
         <div>
-          <p>Nous utilisons des cookies pour améliorer votre expérience. <br /> En utilisant notre site, vous acceptez notre <a href="/privacy-policy" target="blank">politique de confidentialité</a> et l'utilisation de cookies.</p>
+          <p>Nous utilisons des cookies pour améliorer votre expérience. <br /> En utilisant notre site, vous acceptez notre <a href="https://sites.google.com/view/kinomatch-com/accueil" target="blank">politique de confidentialité</a> et l'utilisation de cookies.</p>
           <button onClick={giveCookieConsent}>J'accepte</button>
         </div>
       </div>
